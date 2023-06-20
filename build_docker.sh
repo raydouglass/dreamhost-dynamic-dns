36e878ec9c3c131f8d7972c7911d7494be1de783
Ray Douglass <ray@raydouglass.com>
2023-06-19 20:11:50 -0400

    Switch to buildx & multi-arch

--- build_docker.sh
+++ build_docker.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 
-docker build . -t raydouglass/dreamhost-dynamic-dns:latest
-docker push raydouglass/dreamhost-dynamic-dns:latest
+docker buildx build \
+    --platform=linux/amd64,linux/arm64,linux/ppc64le,linux/s390x,linux/arm/v7,linux/arm/v6 \
+    -t raydouglass/dreamhost-dynamic-dns:latest \
+    --push \
+    .
