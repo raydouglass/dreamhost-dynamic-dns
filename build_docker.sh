6499adffcdc6d7d212dceb4c913df6c888b03095
Ray Douglass <ray@raydouglass.com>
2023-07-05 20:44:05 -0400

    Add linux/arm/v8 to docker build

--- build_docker.sh
+++ build_docker.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 docker buildx build \
-    --platform=linux/amd64,linux/arm64,linux/ppc64le,linux/s390x,linux/arm/v7,linux/arm/v6 \
+    --platform=linux/amd64,linux/arm64,linux/ppc64le,linux/s390x,linux/arm/v8,linux/arm/v7,linux/arm/v6 \
     -t raydouglass/dreamhost-dynamic-dns:latest \
     --push \
     .
